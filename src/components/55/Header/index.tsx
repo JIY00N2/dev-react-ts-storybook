a3837a1b69cb139c9a6282b13538db65f295a9b2
JiYoon <angella990825@gmail.com>
2023-08-17 11:09:33 +0900

    feat: Component - Header

---
+++ src/components/55/Header/index.tsx
@@ -0,0 +1,32 @@
+export interface IHeader {
+  children: React.ReactNode;
+  level?: 1 | 2 | 3 | 4 | 5 | 6;
+  strong?: boolean;
+  underline?: boolean;
+  color?: string;
+}
+
+const Header = ({
+  children,
+  level = 1,
+  strong,
+  underline,
+  color,
+  ...props
+}: IHeader) => {
+  const Tag: keyof HTMLElementTagNameMap = `h${level}`;
+
+  const fontStyle = {
+    fontWeight: strong ? 'bold' : 'normal',
+    textDecoration: underline ? 'underline' : undefined,
+    color,
+  };
+
+  return (
+    <Tag style={{ ...fontStyle, ...props }} {...props}>
+      {children}
+    </Tag>
+  );
+};
+
+export default Header;
---
+++ src/stories/Header.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Header from '../components/55/Header/index';
+
+const meta: Meta<typeof Header> = {
+  component: Header,
+  tags: ['autodocs'],
+  argTypes: {
+    level: { control: { type: 'range', min: 1, max: 6 } },
+    strong: { control: 'boolean' },
+    underline: { control: 'boolean' },
+    color: { control: 'color' },
+  },
+  args: {
+    level: 1,
+    strong: false,
+    underline: false,
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => (
+    <>
+      <Header {...args}>Header</Header>
+    </>
+  ),
+};
