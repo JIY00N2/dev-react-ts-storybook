fd5983b96e02defaa31ccd900ba09b8e84b6d07f
JiYoon <angella990825@gmail.com>
2023-08-21 00:14:20 +0900

    feat: useKeyPress 커스텀 훅

---
+++ src/hooks/useKeyPress.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState, useCallback } from 'react';
+
+const useKeyPress = (targetKey: string) => {
+  const [keyPressed, setKeyPressed] = useState(false);
+
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === targetKey) {
+        setKeyPressed(true);
+      }
+    },
+    [targetKey]
+  );
+
+  const handleKeyUp = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === targetKey) {
+        setKeyPressed(false);
+      }
+    },
+    [targetKey]
+  );
+
+  // useEffect로 전역적으로 이벤트 걸어주기
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, [handleKeyDown, handleKeyUp]);
+
+  return keyPressed;
+};
+
+export default useKeyPress;
