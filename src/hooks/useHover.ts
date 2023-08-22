c41463b467d6050c5ae31193b80bbbc7b6453e72
JiYoon <angella990825@gmail.com>
2023-08-22 13:50:14 +0900

    comment: useHover jw 코드 주석

--- src/hooks/useHover.ts
+++ src/hooks/useHover.ts
@@ -10,6 +10,7 @@ const useHover = (): [React.RefObject<HTMLDivElement>, boolean] => {
   const handelMouseOut = useCallback(() => setState(false), []);
 
   useEffect(() => {
+    if (!ref.current) return;
     const element = ref.current;
     if (!element) return;
     element.addEventListener('mouseover', handelMouseOver);
@@ -24,5 +25,31 @@ const useHover = (): [React.RefObject<HTMLDivElement>, boolean] => {
 
   return [ref, state];
 };
+
+// const useHover = <T extends HTMLElement>(): [
+//   React.MutableRefObject<T | null>,
+//   boolean
+// ] => {
+//   const [state, setState] = useState(false);
+//   const ref = useRef<T | null>(null);
+
+//   const handleMouseOver = useCallback(() => setState(true), []);
+//   const handleMouseOut = useCallback(() => setState(false), []);
+
+//   useEffect(() => {
+//     if (!ref.current) {
+//       return;
+//     }
+//     const element = ref.current;
+//     element.addEventListener('mouseover', handleMouseOver);
+//     element.addEventListener('mouseout', handleMouseOut);
+//     return () => {
+//       element.removeEventListener('mouseover', handleMouseOver);
+//       element.removeEventListener('mouseout', handleMouseOut);
+//     };
+//   }, [handleMouseOver, handleMouseOut]);
+
+//   return [ref, state];
+// };
 
 export default useHover;
