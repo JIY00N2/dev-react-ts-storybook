1efc56815697c0dcfcbc5c87d5ae30eb87390d13
JiYoon <angella990825@gmail.com>
2023-08-24 23:04:44 +0900

    feat: Hook - useTimeoutFn

---
+++ src/hooks/Timeout/useTimeoutFn.ts
@@ -0,0 +1,47 @@
+// 1. 함수 호출을 통한 방법
+// 2. 컴포넌트가 로딩 된 후 바로 실행되는 방법
+
+import { useCallback, useEffect, useRef } from 'react';
+
+// fn: 실행할 함수, ms: 몇 초뒤 실행할 것인지
+const useTimeoutFn = (fn: () => void, ms: number) => {
+  // timeout에 대한 id 기록
+  const timeoutId = useRef<NodeJS.Timeout | undefined>();
+  const callback = useRef(fn);
+
+  useEffect(() => {
+    // 함수가 바뀌었을 경우 처리
+    callback.current = fn;
+  }, [fn]);
+
+  // timeout이 시작되고 끝나면 fn함수를 실행시키는 함수
+  const run = useCallback(() => {
+    // timeoutId가 있다면, clearTimeout을 통해 해제
+    timeoutId.current && clearTimeout(timeoutId.current);
+    // setTimeout 실행
+    timeoutId.current = setTimeout(() => {
+      // fn 함수 실행
+      callback.current();
+    }, ms);
+  }, [ms]);
+
+  // timeout을 해제하는 함수
+  const clear = useCallback(() => {
+    timeoutId.current && clearTimeout(timeoutId.current);
+  }, []);
+
+  // useEffect(() => {
+  //   return () => {
+  //     clear();
+  //   };
+  // }, [clear]);
+
+  // 훅이 사라질 때, clear
+  useEffect(() => {
+    clear;
+  }, [clear]);
+
+  return [run, clear];
+};
+
+export default useTimeoutFn;
---
+++ src/stories/hooks/Timeout/useTimeoutFn.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react';
+import useTimeoutFn from '~/hooks/Timeout/useTimeoutFn';
+
+const meta: Meta<typeof useTimeoutFn> = {
+  title: 'Hook/Timeout/useTimeoutFn ',
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const Component = () => {
+  const [run, clear] = useTimeoutFn(() => {
+    alert('실행');
+  }, 3000);
+  return (
+    <>
+      <div>useTimeoutFn 테스트</div>
+      <button onClick={run}>3초 뒤 실행!</button>
+      <button onClick={clear}>멈춰!</button>
+    </>
+  );
+};
+
+export const Default: Story = {
+  render: () => <Component />,
+};
